8c1ee224f22de42fcbd3193640f9db0f45929995
unknown <rzc307853639@gmail.com>
2020-04-29 10:00:52 +0800

    1

--- src/components/backNavigation.vue
+++ src/components/backNavigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="back" :style="customStyle" :class="{ fixed: isFixed }">
+  <div :style="customStyle" class="back-father">
+    <div class="back" :class="{ fixed: isFixed }">
       <!-- 注意放入插槽 -->
       <font-awesome-icon icon="angle-left" @click="goback" />
       <p class="center"><slot name="title"></slot></p>
@@ -30,38 +30,37 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.back {
+.back-father {
   background-color: white;
-  height: 10vw;
-  line-height: 10vw;
-  display: flex;
-  align-items: center;
   color: rgb(158, 158, 158);
-  svg {
-    font-size: 1.6rem;
-    position: absolute;
+  .back {
+    height: 10vw;
+    line-height: 10vw;
+    display: flex;
+    align-items: center;
+    svg {
+      font-size: 1.6rem;
+      position: absolute;
+    }
+    > .center {
+      text-align: center;
+      width: 100%;
+    }
   }
-  > .center {
-    text-align: center;
+  .fixed {
+    position: fixed;
+    top: 0%;
+    left: 0;
+    right: 0;
+    padding: 0 @padding;
+    z-index: 10;
+  }
+  .position {
     width: 100%;
+    height: 10vw;
+    line-height: 10vw;
+    display: flex;
+    align-items: center;
   }
-}
-.fixed {
-  position: fixed;
-  top: 0%;
-  left: 0;
-  right: 0;
-  background-color: white;
-  padding: 0 @padding;
-  z-index: 10;
-}
-.position {
-  width: 100%;
-  background-color: white;
-  height: 10vw;
-  line-height: 10vw;
-  display: flex;
-  align-items: center;
-  color: rgb(158, 158, 158);
 }
 </style>
--- src/pages/home/location.vue
+++ src/pages/home/location.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="location">
-    asdasdasd
+    <back-navigation
+      :customStyle="{
+        backgroundImage: 'linear-gradient(90deg,#0af,#0085ff)',
+        padding: '0 2vw',
+        color: 'white',
+      }"
+    >
+      <template #title>
+        选择收货地址
+      </template>
+    </back-navigation>
   </div>
 </template>
 
 <script>
+import backNavigation from '@/components/backNavigation';
+import axios from '@/api';
+import { mapActions } from 'vuex';
 export default {
   name: 'location',
+  components: {
+    backNavigation,
+  },
+  created() {
+    const _this = this;
+    axios.get('/location').then((res) => {
+      const data = res.data;
+      _this.initLocaionList(data);
+      _this.changeStatus(200);
+    });
+  },
+  methods: {
+    ...mapActions('home/location', ['initLocaionList', 'changeStatus']),
+  },
 };
 </script>
 
 <style lang="less" scoped>
 .location {
-  padding: 0 @padding;
   background-color: white;
 }
 </style>
