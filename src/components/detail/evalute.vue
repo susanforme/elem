c91f22184d59f63d7b929d15dc9dfa394efabaa6
unknown <rzc307853639@gmail.com>
2020-05-03 13:52:21 +0800

    1

--- src/components/detail/evalute.vue
+++ src/components/detail/evalute.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    evalue
+  <div class="evalute">
+    <score-rank :rating="comments.rating"></score-rank>
   </div>
 </template>
 
 <script>
+import scoreRank from './scoreRank';
 export default {
   props: {
     comments: {
       type: Object,
     },
+  },
+  components: {
+    scoreRank,
   },
 };
 </script>
--- src/components/detail/scoreRank.vue
+++ src/components/detail/scoreRank.vue
@@ -1,11 +1,91 @@
 <template>
-  <div>
-    score
+  <div class="score">
+    <div class="score-head">{{ score | toFix }}</div>
+    <div class="busine-score">
+      <p class="title">商家评分</p>
+      <p class="child"><stars :score="score"></stars></p>
+    </div>
+    <div class="overall">
+      <p class="title">味道</p>
+      <p class="child">{{ rating.overall_score | toFix }}</p>
+    </div>
+    <div class="pack">
+      <p class="title">包装</p>
+      <p class="child">{{ rating.shop_score | toFix }}</p>
+    </div>
+    <div class="divery">
+      <p class="title">配送</p>
+      <p class="child">{{ rating.shop_score | toFix }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import stars from '@/components/stars';
+export default {
+  props: {
+    rating: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    score() {
+      return (this.rating.overall_score + this.rating.shop_score) / 2;
+    },
+  },
+  filters: {
+    toFix(value) {
+      return value.toFixed(1);
+    },
+  },
+  components: {
+    stars,
+  },
+};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.score {
+  background-color: white;
+  display: flex;
+  height: 24vw;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  color: @gray;
+  > * {
+    flex: 1;
+  }
+  > div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    > p {
+      flex: 1;
+    }
+  }
+  > .score-head {
+    font-size: 32px;
+    font-weight: 600;
+    color: #ff6000;
+    flex: 1.5;
+  }
+  .overall,
+  .pack {
+    > p.child {
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
+  > .divery {
+    flex: 1.3;
+    border-left: 1px solid rgba(202, 202, 202, 0.616);
+    > .child {
+      font-weight: 700;
+      font-size: 18px;
+    }
+  }
+}
+</style>
